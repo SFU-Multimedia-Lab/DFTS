3c6fa3f92279c86ba5a2eac5fa0b61074dfd610c
harshanavkis <harshanavkis@gmail.com>
2018-05-11 12:31:46 -0700

    Added simulation backbone

--- main.py
+++ main.py
@@ -1,21 +1,37 @@
 import argparse
+import numpy as np
+import simmods
+import utils
 
 def argumentReader():
     '''
         * Read in command line arguments
             ** m: model such as those given at https://keras.io/applications/
             ** l: layer you want to split your model at
+            ** i: path to the image
     '''
     ap = argparse.ArgumentParser()
     ap.add_argument("-m", "--model", help="name of the model", required=True)
     ap.add_argument("-l", "--layer", help="layer you want to split at", required=True)
+    ap.add_argument("-i", "--image", help="path to the image")
     args = vars(ap.parse_args())
     return args
 
 def runSimulation():
     args = argumentReader()
-    print(args['model'], args['layer'])
-
+    print(args['model'], args['layer'], args['image'])
+    '''
+        * Part of the model runs on the device: deviceSim
+        * Compression: compress
+        * Channel: transmit
+        * Other Part runs in the cloud: remoteSim
+    '''
+    deviceOut, actualOut = simmods.deviceSim(args['model'], args['layer'], args['image'])
+    print("hello")
+    compressOut          = simmods.compress(deviceOut)
+    channelOut           = simmods.transmit()
+    remoteOut            = simmods.remoteSim(channelOut)
+    utils.errorCalc(remoteOut, actualOut)
 
 if __name__ == '__main__':
     runSimulation()
---
+++ simmods.py
@@ -0,0 +1,19 @@
+modelDict = {'xception':'Xception', 'vgg16':'vgg16', 'VGG19':'vgg19', 'resnet50':'ResNet50',
+             'inceptionv3':'InceptionV3', 'inceptionresnetv2':'InceptionResnetV2',
+             'mobilenet':'MobileNet', 'densenet':'DenseNet','nasnet':'NASNet'}
+
+def deviceSim(model, layer, testImage):
+    '''
+        *
+    '''
+    # print(modelDict)
+    # return (2,3)
+
+def compress(deviceOut):
+    pass
+
+def transmit():
+    pass
+
+def remoteSim(channelOut):
+    pass
