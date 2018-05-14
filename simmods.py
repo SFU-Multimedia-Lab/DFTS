694a25afeafcbdcfb8ede5ccabf9a2c50f50f015
harshanavkis <harshanavkis@gmail.com>
2018-05-14 11:21:46 -0700

    dynamically load model and functions

--- simmods.py
+++ simmods.py
@@ -1,7 +1,8 @@
 from utils import preprocess
+import keras
 import sys
 
-modelDict = {'xception':'Xception', 'vgg16':'vgg16', 'VGG19':'vgg19', 'resnet50':'ResNet50',
+modelDict = {'xception':'Xception', 'vgg16':'VGG16', 'VGG19':'VGG19', 'resnet50':'ResNet50',
              'inceptionv3':'InceptionV3', 'inceptionresnetv2':'InceptionResnetV2',
              'mobilenet':'MobileNet', 'densenet':'DenseNet','nasnet':'NASNet'}
 
@@ -18,7 +19,9 @@ def deviceSim(model, layer, testImage):
         print("We currently do not support that model. Please try a valid one.")
         sys.exit(1)
     preprocessedImage = preprocess(testImage, model)
-    print(preprocessedImage)
+    exec("from keras.applications."+model.lower()+" import "+model)
+    convModel = getattr(keras.applications,f"{model}")(weights='imagenet', include_top=False)
+    print(convModel.summary())
     return (2,3)
 
 def compress(deviceOut):
