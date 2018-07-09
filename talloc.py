5aa5b5e979d15ad29a817ad28efed45fd468b002
harshanavkis <harshanavkis@gmail.com>
2018-07-09 13:22:21 -0700

    Add task allocater

---
+++ talloc.py
@@ -0,0 +1,14 @@
+from modelTasks.tasks import .
+
+def taskAllocater(task, testDir, reshapeDims, batch_size, normalize):
+    # task is an integer representing the model's application
+    #each task is an object having the following attributes:
+        #data generator: different according to the structure of the testInput dir
+        #data preprocessing
+    #0: classification
+    #1: object detection
+
+    if task==0:
+        return CFTask(testDir, reshapeDims, batch_size, normalize)
+    if task==1:
+        return ODTask(testDir)
